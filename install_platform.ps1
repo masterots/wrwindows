5dd6545b6cd29219407b56362d9225ace9b3a8e2
Joshua Melvin <joshua.melvin@gmail.com>
2012-06-25 14:31:31 -0500

    config changes / install path changes

--- install_platform.ps1
+++ install_platform.ps1
@@ -1,8 +1,8 @@
 ﻿##### SET SOME GLOBAL VARIABLES #####
 $rootDrive = "C:"
-$javaHome = "C:\java\jre"
 $svnworkHome = $rootDrive + "\svnwork"
 $toolsHome = $rootDrive + "\tools"
+$javaHome = $toolsHome + "\java\jdk"
 $mavenHome = $toolsHome + "\maven"
 $userHome = $env:USERPROFILE
 $time = Get-Date -Format r
@@ -82,7 +82,7 @@ function setUpBazaarvoiceHome {
             logEvent "Created $rootDrive\home\bazaarvoice\$dir"
         }
     }
-	
+
 	logEvent "Setting up config-branch... "
 	cd $rootDrive\home\bazaarvoice\config-branch 
 	New-Item -Type directory -Path 10-localhost 
@@ -91,7 +91,7 @@ function setUpBazaarvoiceHome {
 	createSymLink 20-overrides-dev $svnworkHome\prr\branch\working\metadata\home\config\20-overrides-dev 
 	createSymLink 25-overrides-dev-lab-austin $svnworkHome\prr\branch\working\metadata\home\config\25-overrides-dev-lab-austin 
 	createSymLink 30-common $svnworkHome\prr\branch\working\metadata\home\config\30-common 
-	
+
 	logEvent "Setting up config-trunk... "
 	cd $rootDrive\home\bazaarvoice\config-trunk 
 	New-Item -Type directory -Path 10-localhost 
@@ -112,16 +112,16 @@ function setUpBazaarvoiceHome {
 	copy-item $svnworkHome\techservices\trunk\working\tools\misc\dev-install\host_overrides.properties $rootDrive\home\bazaarvoice\config-branch\10-localhost
 	copy-item $svnworkHome\techservices\trunk\working\tools\misc\dev-install\portal_endpoints.properties $rootDrive\home\bazaarvoice\config-branch\10-localhost
 	copy-item $svnworkHome\techservices\trunk\working\tools\misc\dev-install\jetty.properties $rootDrive\home\bazaarvoice\config-branch\10-localhost
-	
+
 	logEvent "Copying env.xml host_overrides.properties, portal_endpoints.properties, and jetty.properties for config-trunk... "
 	copy-item $svnworkHome\techservices\trunk\working\tools\misc\dev-install\env.xml $rootDrive\home\bazaarvoice\config-trunk\10-localhost
 	copy-item $svnworkHome\techservices\trunk\working\tools\misc\dev-install\host_overrides.properties $rootDrive\home\bazaarvoice\config-trunk\10-localhost
 	copy-item $svnworkHome\techservices\trunk\working\tools\misc\dev-install\portal_endpoints.properties $rootDrive\home\bazaarvoice\config-trunk\10-localhost
 	copy-item $svnworkHome\techservices\trunk\working\tools\misc\dev-install\jetty.properties $rootDrive\home\bazaarvoice\config-trunk\10-localhost
-	
+
 	logEvent "Writing production_secret_key.bin for config-branch... "
 	Write-Output "3c774ac04349bbbf861cd59cb7594d09892139227b3dda3411e3f0cdf96da54a" > $rootDrive\home\bazaarvoice\config-branch\production_secret_key.bin
-	
+
 	logEvent "Writing production_secret_key.bin for config-trunk... "
 	Write-Output "3c774ac04349bbbf861cd59cb7594d09892139227b3dda3411e3f0cdf96da54a" > $rootDrive\home\bazaarvoice\config-trunk\production_secret_key.bin
 }
@@ -158,7 +158,7 @@ function configureSubversion {
 
 function installJava {
     logEvent ("Installing java")
-    C:\temp\installers\jdk-6u30-windows-x64.exe /s INSTALLDIR=$toolsHome\java\jdk
+    C:\temp\installers\jdk-6u30-windows-x64.exe /s INSTALLDIR=$javaHome
     logEvent ("Wait 3 minutes")
     Start-Sleep -s 180
     logEvent ("Completed installing java")
@@ -170,7 +170,7 @@ function installJunction {
     cd $toolsHome\junction
     $filename = "junction-current.zip" 
     $shell_app=new-object -com shell.application  
-    $zip_file = $shell_app.namespace("$rootDrive\temp\$filename") 
+    $zip_file = $shell_app.namespace("$rootDrive\temp\installers\$filename") 
     $destination = $shell_app.namespace((Get-Location).Path) 
     $destination.Copyhere($zip_file.items())
     logEvent ("Completed installing junction")
@@ -181,16 +181,10 @@ function installMaven {
     cd $toolsHome
     $filename = "maven-current.zip"
     $shell_app=new-object -com shell.application  
-    $zip_file = $shell_app.namespace("$rootDrive\temp\$filename") 
+    $zip_file = $shell_app.namespace("$rootDrive\temp\installers\$filename") 
     $destination = $shell_app.namespace((Get-Location).Path) 
     $destination.Copyhere($zip_file.items())
     Get-ChildItem -Filter "*maven*" | Rename-Item -NewName "maven"
-
-    New-Item -Type directory -Path ($userHome+"\.m2")
-    Copy-Item ($svnworkHome+"\techservices\trunk\working\tools\bin\maven-install\ts-maven-settings.xml") ($userHome+"\.m2\settings.xml")
-    New-Item -Type file -Path ($userHome+"\.mavenrc")
-    echo MAVEN_OPTS='"-Xms256m -Xmx2g -XX:MaxPermSize=256m -server"' > ($userHome+"\.mavenrc")
-    logEvent ("Completed installing maven")
 }
 
 function installAnt {
@@ -199,7 +193,7 @@ function installAnt {
     $filename = "ant-current.zip"
     #copy-item \Users\joshua.melvin\Downloads\$filename (Get-Location).Path
     $shell_app=new-object -com shell.application  
-    $zip_file = $shell_app.namespace("$rootDrive\temp\$filename") 
+    $zip_file = $shell_app.namespace("$rootDrive\temp\installers\$filename") 
     $destination = $shell_app.namespace((Get-Location).Path) 
     $destination.Copyhere($zip_file.items())
     Get-ChildItem -Filter "*ant*" | Rename-Item -NewName "ant"
@@ -211,7 +205,7 @@ function installXmlBeans {
     cd $toolsHome
     $filename = "xmlbeans-current.zip"
     $shell_app=new-object -com shell.application  
-    $zip_file = $shell_app.namespace("$filename") 
+    $zip_file = $shell_app.namespace("$rootDrive\temp\installers\$filename") 
     $destination = $shell_app.namespace((Get-Location).Path) 
     $destination.Copyhere($zip_file.items())
     Get-ChildItem -Filter "*xmlbeans*" | Rename-Item -NewName "xmlbeans"
@@ -240,12 +234,12 @@ function addEnvSetup {
 
 
 ##### BEGIN INSTALLATION #####
+New-Item -Type directory -Path $svnworkHome
 Write-Output "Windows Bazaarvoice Dev Environment Setup Script" > $logfile
 logEvent "Started install at"
 
 installTools
 addEnvSetup
-setupDirectories
 
 ##### GET LATEST BRANCH VERSION #####
 $branchVersion = svn ls $branchUrl | Sort-Object -Descending | Select-Object -First 1 -Skip 1 | %{$_ -replace '/','' }
@@ -267,11 +261,21 @@ createSymLink -linkName branch -linkUrl $svnworkHome\prr\$branchVersion
 getFiles -remoteUrl $defaultUiBranchUrl -localFolder defaultui -currentBranchVersion $branchVersion -workingClean clean
 createSymLink -linkName branch -linkUrl $svnworkHome\defaultui\$branchVersion
 
+setUpBazaarvoiceHome
+
 # INSTALL LOCAL LDAP CERTIFICATE
 logEvent "Installing local LDAP certificate... "
 keytool.exe -importkeystore -alias shareddev -storepass "changeit" -keystore $env:JAVA_HOME\jre\lib\security\cacerts -file $svnworkHome\techservices\trunk\working\tools\misc\dev-install\labCA.pem
 logEvent "  Installing local Maven certificate... "
 keytool.exe -importkeystore -alias bvrepo -storepass "changeit" -keystore $env:JAVA_HOME\jre\lib\security\cacerts -file $svnworkHome\techservices\trunk\working\tools\misc\dev-install\repo-ssl.cer
+
+##### CONFIGURE MAVEN #####
+New-Item -Type directory -Path ($userHome+"\.m2")
+Copy-Item ($svnworkHome+"\techservices\trunk\working\tools\bin\maven-install\ts-maven-settings.xml") ($userHome+"\.m2\settings.xml")
+New-Item -Type file -Path ($userHome+"\.mavenrc")
+echo MAVEN_OPTS='"-Xms256m -Xmx2g -XX:MaxPermSize=256m -server"' > ($userHome+"\.mavenrc")
+logEvent ("Completed installing maven")
+
 
 logEvent "Install completed"
  
